0ce192c7581d7fe8f600f04c0897d6934a70d3bd
Nate West <natescott.west@gmail.com>
2014-08-02 06:20:30 -0500

    Add deps

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject budget "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject budget "0.0.1"
+  :description "Simple budget tracking for myself"
+  :url "http://github.com/niftyn8/budget"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [cheshire "5.3.1"]
+                 [hiccup-bootstrap-3 "0.2.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler budget.handler/app
          :init budget.handler/init
